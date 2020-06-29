29bb0687704fa7a73340ea8b03acc57069c244af
Shady <390584929@qq.com>
2020-06-29 21:03:22 +0800

    Member

--- Store-Member/src/main/java/org/example/Member.java
+++ Store-Member/src/main/java/org/example/Member.java
@@ -2,12 +2,15 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  * @author Shady
  */
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Member {
     public static void main(String[] args) {
