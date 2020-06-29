f1ae36b48be0d74750403a95780830ed4d2cbc76
Shady <390584929@qq.com>
2020-06-29 20:41:06 +0800

    Member

---
+++ Store-Member/src/main/java/org/example/Member.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author Shady
+ */
+@SpringBootApplication
+public class Member {
+    public static void main(String[] args) {
+        SpringApplication.run(Member.class, args);
+    }
+}
