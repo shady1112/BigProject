e57e5f7c360816fa0420e44b5cb64f84eca78e0b
liujunyi <991697843@qq.com>
2020-06-29 20:52:12 +0800

    提交测试

--- Store-Member/src/main/java/org/example/Member.java
+++ Store-Member/src/main/java/org/example/Member.java
@@ -11,6 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Member {
     public static void main(String[] args) {
-        SpringApplication.run(Member.class, args);
+        SpringApplication.run(Member.class, args);  //提交测试注释
     }
 }
