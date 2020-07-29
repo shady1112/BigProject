e3d2cbda77ec0375444c4682e3662a5bda06d869
Shady <390584929@qq.com>
2020-07-29 22:43:21 +0800

    Member

--- Store-Member/src/main/java/org/example/controller/LoginController.java
+++ Store-Member/src/main/java/org/example/controller/LoginController.java
@@ -1,24 +1,16 @@
 package org.example.controller;
 
 
-import com.alibaba.fastjson.JSONObject;
 import org.example.bean.Users;
 import org.example.common.Result;
-import org.example.common.ResultUtil;
-import org.example.common.State;
-import org.example.common.TokenUtil;
 import org.example.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
-/**
- * @author Shady
- */
+
 @CrossOrigin
 @RequestMapping("login")
 @RestController
@@ -39,5 +31,14 @@ public class LoginController {
         return loginService.selectUserCount(user);
     }
 
+    /**
+     * 这是用户登录方法
+     *
+     * @param user 用户信息
+     */
+    @RequestMapping("userRegistry")
+    public Result userRegistry(Users user) {
+        return loginService.selectUserCount(user);
+    }
 
 }
